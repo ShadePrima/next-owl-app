1d89e985ce8149c62e4b4400d818f4cd1a7b0383
ShadePrima <aleksandrag1984@gmail.com>
2023-06-15 12:54:53 +0300

    fix concept slider

--- app/components/conceptMobileCard/ConceptMobileCard.tsx
+++ app/components/conceptMobileCard/ConceptMobileCard.tsx
@@ -23,36 +23,10 @@ export default function ConceptMobileCard() {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     initialSlide: 0,
     arrows: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
   }
 
   return (
